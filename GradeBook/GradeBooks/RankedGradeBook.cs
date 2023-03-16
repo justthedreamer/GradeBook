ca664f9541e50b29dd9d5028e45e970d88721334
Piotr Zapała <piotr.zapala.dev@gmail.com>
2023-03-16 19:16:25 +0100

    Task #4:Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,39 @@ namespace GradeBook.GradeBooks
         {
             Type=Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count >= 5)
+            {
+                var AtopOfTheClass = (Students.Count * 0.2);
+                var BtopOfTheClass = (Students.Count * 0.4);
+                var CtopOfTheClass = (Students.Count * 0.6);
+                var DtopOfTheClass = (Students.Count * 0.8);
+                List<double> avarageRanking = new List<double>();
+
+                foreach (var student in Students)
+                {
+                    avarageRanking.Add(student.AverageGrade);
+                }
+                avarageRanking.Sort();
+                avarageRanking.Reverse();
+                int index = avarageRanking.IndexOf(averageGrade);
+
+
+                if (index < AtopOfTheClass)
+                    return 'A';
+                else if (index >= AtopOfTheClass && index < BtopOfTheClass)
+                    return 'B';
+                else if (index >= BtopOfTheClass && index < CtopOfTheClass)
+                    return 'C';
+                else if (index >= CtopOfTheClass && index < DtopOfTheClass)
+                    return 'D';
+                else
+                    return 'F';
+
+            }
+            else throw new InvalidOperationException();
+        }
+
     }
 }
